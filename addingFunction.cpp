b180a8048fdf10453a8e9254c7411bb0b32ada24
NathanShaw02 <nathanshaw02@outlook.com>
2025-01-13 16:29:53 +0000

    Using pointers to modify out of scope variables

---
+++ addingFunction.cpp
@@ -0,0 +1,11 @@
+/*Goal: practice writing functions with parameters and return values.*/
+
+#include "addingFunction.hpp"
+
+int main(){
+    
+    printEquation(5.0,4.0,sum(5.0,4.0),'+');
+
+    return 0;
+    
+}
---
+++ calcAndPrintHPP.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void calculate(float input1,float input2,char operation,float &result){
+
+    switch(operation)
+    {
+        case '+': result = input1 + input2;
+                  break;
+        case '-': result = input1 - input2;
+                  break;   
+        case '*': result = input1*  input2;
+                  break;
+        case '/': result = input1 / input2;
+                  break;
+        default:  std::cout<<"Illegal operation\n";
+    }
+
+}
+
+
+void printEquation(float input1,float input2,char operation,float result){
+
+    std::cout<<input1<<" "<<operation<<" "<<input2<< " = "<<result;
+
+}
